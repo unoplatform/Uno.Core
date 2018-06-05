6b2ceb3efa35517f0ba9453dcd0757c996d8c612
Dr.Rx <david.rey@nventive.com>
2018-06-05 10:06:48 -0400

    Add the `FastAsyncLock` Compared to `AsyncLock`, it - Ensure to continue other awaiters synchronously instead of releasing them on an uncontrolled context. - Allow re-entrency Also - Import tests of the `FastTaskCompletionSource`

---
+++ src/Uno.Core.Tests/Threading/FastTaskCompletionSourceFixture.cs
@@ -0,0 +1,445 @@
+// ******************************************************************
+// Copyright � 2015-2018 nventive inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ******************************************************************
+using System;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.Core.Tests.TestUtils;
+using Uno.Threading;
+
+namespace Uno.Core.Tests.Threading
+{
+	[TestClass]
+	public class FastTaskCompletionSourceFixture
+	{
+		[TestMethod]
+		public void TestSetResultSynchronously1()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			sut.SetResult("1234");
+
+			// The task is created AFTER the result being set
+			var task = sut.Task;
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.RanToCompletion);
+			task.Result.Should().BeEquivalentTo("1234");
+		}
+
+		[TestMethod]
+		public void TestSetResultSynchronously2()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			// The task is created BEFORE the result being set
+			var task = sut.Task;
+			task.IsCompleted.Should().BeFalse();
+
+			sut.SetResult("1234");
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.RanToCompletion);
+			task.Result.Should().BeEquivalentTo("1234");
+		}
+
+		[TestMethod]
+		public void TestSetResultAsynchronously()
+		{
+			var scheduler = new TestScheduler();
+			var sut = new FastTaskCompletionSource<string>();
+
+			var threadId = Thread.CurrentThread.ManagedThreadId;
+
+			var task = scheduler
+				.Run(async ct =>
+				{
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					var r = await sut.Task;
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					return r;
+				}, CancellationToken.None);
+
+			task.IsCompleted.Should().BeFalse();
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeFalse();
+			sut.SetResult("1234");
+
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.RanToCompletion);
+			task.Result.Should().BeEquivalentTo("1234");
+
+			scheduler.Stop();
+		}
+
+		[TestMethod]
+		public void TestSetResultAsynchronously_AndAwaitDirectly1()
+		{
+			var scheduler = new TestScheduler();
+			var sut = new FastTaskCompletionSource<string>();
+
+			var threadId = Thread.CurrentThread.ManagedThreadId;
+
+			var task = scheduler
+				.Run(async ct =>
+				{
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					var r = await sut;
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					return r;
+				}, CancellationToken.None);
+
+			task.IsCompleted.Should().BeFalse();
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeFalse();
+			sut.SetResult("1234");
+
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.RanToCompletion);
+			task.Result.Should().BeEquivalentTo("1234");
+
+			scheduler.Stop();
+		}
+
+		[TestMethod]
+		public void TestSetResultAsynchronously_AndAwaitDirectly2()
+		{
+			var scheduler = new TestScheduler();
+			var sut = new FastTaskCompletionSource<string>();
+
+			var threadId = Thread.CurrentThread.ManagedThreadId;
+
+			sut.SetResult("1234");
+
+			scheduler.AdvanceBy(5);
+
+			var task = scheduler
+				.Run(async ct =>
+				{
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					var r = await sut;
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					return r;
+				}, CancellationToken.None);
+
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.RanToCompletion);
+			task.Result.Should().BeEquivalentTo("1234");
+
+			scheduler.Stop();
+		}
+
+		[TestMethod]
+		public void TestSetCanceledSynchronously1()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			sut.SetCanceled();
+
+			// The task is created AFTER the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Canceled);
+		}
+
+		[TestMethod]
+		public void TestSetCanceledSynchronously2()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			// The task is created BEFORE the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeFalse();
+
+			sut.SetCanceled();
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Canceled);
+		}
+
+		[TestMethod]
+		public void TestSetCanceledAsynchronously()
+		{
+			var scheduler = new TestScheduler();
+			var sut = new FastTaskCompletionSource<string>();
+
+			var threadId = Thread.CurrentThread.ManagedThreadId;
+
+			var task = scheduler
+				.Run(async ct =>
+				{
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					await sut.Task;
+					throw new Exception("should not reach here");
+				}, CancellationToken.None);
+
+			task.IsCompleted.Should().BeFalse();
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeFalse();
+
+			sut.SetCanceled();
+
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Canceled);
+
+			scheduler.Stop();
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously1()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			try
+			{
+				throw new ArgumentNullException("xxx");
+			}
+			catch (ArgumentNullException ex)
+			{
+				sut.SetException(ex);
+			}
+
+			// The task is created AFTER the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Faulted);
+			task.Exception.Should().NotBeNull();
+			var aggregateException = task.Exception;
+			aggregateException.InnerExceptions.Should().HaveCount(1);
+			var argException = aggregateException.InnerException;
+			argException.Should().BeOfType<ArgumentNullException>();
+			argException.TargetSite.Name.Should().BeEquivalentTo(nameof(TestSetExceptionSynchronously1), "original context is lost");
+
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously2()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			// The task is created BEFORE the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeFalse();
+
+			try
+			{
+				throw new ArgumentNullException("xxx");
+			}
+			catch (ArgumentNullException ex)
+			{
+				sut.SetException(ex);
+			}
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Faulted);
+			task.Exception.Should().NotBeNull();
+			var aggregateException = task.Exception;
+			aggregateException.InnerExceptions.Should().HaveCount(1);
+			var argException = aggregateException.InnerException;
+			argException.Should().BeOfType<ArgumentNullException>();
+			argException.TargetSite.Name.Should().BeEquivalentTo(nameof(TestSetExceptionSynchronously2), "original context is lost");
+		}
+
+		[TestMethod]
+		public void TestSetExceptionAsynchronously()
+		{
+			var scheduler = new TestScheduler();
+			var sut = new FastTaskCompletionSource<string>();
+
+			var threadId = Thread.CurrentThread.ManagedThreadId;
+
+			var task = scheduler
+				.Run(async ct =>
+				{
+					Thread.CurrentThread.ManagedThreadId.Should().Be(threadId);
+					await sut.Task;
+					throw new Exception("should not reach here");
+				}, CancellationToken.None);
+
+			task.IsCompleted.Should().BeFalse();
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeFalse();
+
+			try
+			{
+				throw new ArgumentNullException("xxx");
+			}
+			catch (ArgumentNullException ex)
+			{
+				sut.SetException(ex);
+			}
+
+			scheduler.AdvanceBy(5);
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Faulted);
+			task.Exception.Should().NotBeNull();
+			var aggregateException = task.Exception;
+			aggregateException.InnerExceptions.Should().HaveCount(1);
+			var argException = aggregateException.InnerException;
+			argException.Should().BeOfType<ArgumentNullException>();
+			argException.TargetSite.Name.Should().BeEquivalentTo(nameof(TestSetExceptionAsynchronously), "original context is lost");
+
+			scheduler.Stop();
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously_UsingExceptionDispatchInfo1()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			try
+			{
+				throw new ArgumentNullException("xxx");
+			}
+			catch (ArgumentNullException ex)
+			{
+				sut.SetException(ExceptionDispatchInfo.Capture(ex));
+			}
+
+			// The task is created AFTER the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Faulted);
+			task.Exception.Should().NotBeNull();
+			var aggregateException = task.Exception;
+			aggregateException.InnerExceptions.Should().HaveCount(1);
+			var argException = aggregateException.InnerException;
+			argException.Should().BeOfType<ArgumentNullException>();
+			argException.TargetSite.Name.Should().BeEquivalentTo(nameof(TestSetExceptionSynchronously_UsingExceptionDispatchInfo1), "original context is lost");
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously_UsingExceptionDispatchInfo2()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			// The task is created BEFORE the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeFalse();
+
+			try
+			{
+				throw new ArgumentNullException("xxx");
+			}
+			catch (ArgumentNullException ex)
+			{
+				sut.SetException(ExceptionDispatchInfo.Capture(ex));
+			}
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Faulted);
+			task.Exception.Should().NotBeNull();
+			var aggregateException = task.Exception;
+			aggregateException.InnerExceptions.Should().HaveCount(1);
+			var argException = aggregateException.InnerException;
+			argException.Should().BeOfType<ArgumentNullException>();
+			argException.TargetSite.Name.Should().BeEquivalentTo(nameof(TestSetExceptionSynchronously_UsingExceptionDispatchInfo2), "original context is lost");
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously_WithTaskCanceledException1()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			// The task is created BEFORE the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeFalse();
+
+			try
+			{
+				throw new TaskCanceledException("xxx");
+			}
+			catch (TaskCanceledException ex)
+			{
+				sut.SetException(ex);
+			}
+
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Canceled);
+			task.Exception.Should().BeNull();
+		}
+
+		[TestMethod]
+		public void TestSetExceptionSynchronously_WithTaskCanceledException2()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			try
+			{
+				throw new TaskCanceledException("xxx");
+			}
+			catch (TaskCanceledException ex)
+			{
+				sut.SetException(ex);
+			}
+
+			// The task is created AFTER the task being canceled
+			var task = sut.Task;
+			task.IsCompleted.Should().BeTrue();
+			task.Status.Should().Be(TaskStatus.Canceled);
+			task.Exception.Should().BeNull();
+		}
+
+		[TestMethod]
+		public void TestSetResultTwice_ThrowException()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			Action action = () => sut.SetResult("1234");
+
+			action.Should().NotThrow();
+			action.Should().Throw<InvalidOperationException>();
+		}
+
+		[TestMethod]
+		public void TestSetCanceledTwice_ThrowException()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			Action action = () => sut.SetCanceled();
+
+			action.Should().NotThrow();
+			action.Should().Throw<InvalidOperationException>();
+		}
+
+		[TestMethod]
+		public void TestSetExceptionTwice_ThrowException()
+		{
+			var sut = new FastTaskCompletionSource<string>();
+
+			Action action = () => sut.SetException(new ArgumentNullException("xxx"));
+
+			action.Should().NotThrow();
+			action.Should().Throw<InvalidOperationException>();
+		}
+	}
+}
