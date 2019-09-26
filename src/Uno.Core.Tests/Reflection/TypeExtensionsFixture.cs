12333234502590a078fa2c58b067e83d77f1634e
David Oliver <david.oliver@nventive.com>
2019-09-25 21:42:14 -0400

    Fix GetBaseTypes() extension
    Was only ever returning the first base type.

---
+++ src/Uno.Core.Tests/Reflection/TypeExtensionsFixture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.Extensions;
+
+namespace Uno.Core.Tests.Reflection
+{
+	[TestClass]
+	public class TypeExtensionsFixture
+	{
+		[TestMethod]
+		public void TestGetBaseTypes()
+		{
+			var baseTypes = typeof(MoreDerivedType).GetBaseTypes().ToArray();
+			CollectionAssert.AreEqual(new[] { typeof(DerivedType), typeof(BaseType), typeof(object) }, baseTypes);
+		}
+
+		public class BaseType { }
+
+		public class DerivedType : BaseType { }
+
+		public class MoreDerivedType : DerivedType { }
+	}
+}
